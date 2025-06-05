28682acd38394a759913c7bb1d32f051805ffc89
Gabriel Luis Freitas <gabiluisfreitas@gmail.com>
2025-06-05 13:53:37 +0200

    Update footer text to reflect branding change from 'RSS Feed' to 'Monopress'

--- src/components/shared/layout/footer.tsx
+++ src/components/shared/layout/footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="border-t py-6">
       <div className="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-between gap-4 px-4 md:flex-row md:px-8">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          @2025 RSS Feed Built with ❤️ by Gabriel Luis
+          @2025 Monopress - Built with ❤️ by Gabriel Luis
         </p>
       </div>
     </footer>
